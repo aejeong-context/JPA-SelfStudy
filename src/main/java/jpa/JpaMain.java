bd570c263185126145c2b9c9e26fe30a4dded337
saj <aejeong-context@kakao.com>
2021-05-13 09:58:39 +0900

    [add] JPQL 작성
    객체를 대상으로한 쿼리이다.
    DB에 따라 알맞게 번역해준다.(문법)

--- src/main/java/jpa/JpaMain.java
+++ src/main/java/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -14,8 +15,17 @@ public class JpaMain {
     tx.begin();
     try {
 
-      Member member = em.find(Member.class, 1L);
-      member.setName("HELLO");
+      //      Member member = em.find(Member.class, 1L);
+      // JPQL 객체를 대상으로한 쿼리이기 때문에 방언에 맞춰서 각 DB에 맞게 변역을 해준다.
+      List<Member> result =
+          em.createQuery("select m from Member m", Member.class)
+              .setFirstResult(8)
+              .setMaxResults(5)
+              .getResultList();
+
+      for (Member member : result) {
+        System.out.println(member);
+      }
 
       tx.commit();
     } catch (Exception e) {
