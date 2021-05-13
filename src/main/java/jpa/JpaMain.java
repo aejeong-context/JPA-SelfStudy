fad88f93ea4ef0a06829ccccf839d39ba2007d28
saj <aejeong-context@kakao.com>
2021-05-13 14:57:46 +0900

    [add] 기본 키 매핑 - GenerationType
    IDENTITY 를 사용하여 ID 자동으로 추가

--- src/main/java/jpa/JpaMain.java
+++ src/main/java/jpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -14,6 +13,12 @@ public class JpaMain {
     EntityTransaction tx = em.getTransaction();
     tx.begin();
     try {
+
+      // id 입력하지 않아도 자동으로 생성 -> generatedType.IDENTITY 설정
+      Member member = new Member();
+      member.setUsername("aejeong");
+      em.persist(member);
+
 
 
       tx.commit();
--- src/main/java/jpa/Member.java
+++ src/main/java/jpa/Member.java
@@ -1,62 +1,30 @@
 package jpa;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 public class Member {
 
-  @Id private Long id;
-  private String name;
-  private Integer age;
-  @Enumerated(EnumType.STRING)
-  private RoleType roleType;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date createDate;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date modified;
-  @Lob
-  private String description;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  // 기본 생성자가 있어야 한다.
-  public Member() {
-  }
-
-  public Member(Long id, String name, Integer age, RoleType roleType, Date createDate, Date modified, String description) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.roleType = roleType;
-    this.createDate = createDate;
-    this.modified = modified;
-    this.description = description;
-  }
+  @Column(name = "name", nullable = false)
+  private String username;
 
   public Long getId() {
     return id;
   }
 
-  public String getName() {
-    return name;
+  public String getUsername() {
+    return username;
   }
 
-  public Integer getAge() {
-    return age;
+  public void setId(Long id) {
+    this.id = id;
   }
 
-  public RoleType getRoleType() {
-    return roleType;
-  }
-
-  public Date getCreateDate() {
-    return createDate;
-  }
-
-  public Date getModified() {
-    return modified;
-  }
-
-  public String getDescription() {
-    return description;
+  public void setUsername(String username) {
+    this.username = username;
   }
 }
